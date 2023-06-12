7fd9587cc7281e95012e827c4a193b3e7f0a434c
Anna Kucherenko <kucherenko.place@icloud.com>
2023-06-12 13:31:06 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import { Global, ThemeProvider } from '@emotion/react';
-import { App } from './components/App';
-import { GlobalStyles, theme } from './styles';
+import { App } from 'components/App';
+import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-      <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyles} />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename="/goit-react-hw-05-movies">
+            <ThemeProvider theme={theme}>
+                <Global styles={GlobalStyles} />
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );

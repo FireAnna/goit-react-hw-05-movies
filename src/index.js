9f9f30d08afdf0ddee8f9d0166815c0353516fcc
Anna Kucherenko <kucherenko.place@icloud.com>
2023-06-12 11:06:41 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import { Global, ThemeProvider } from '@emotion/react';
-import { App } from 'components/App';
+import { App } from './components/App';
 import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
